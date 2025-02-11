2c0f81a31597332bc5578bdab1adc14f3347597a
Shahzodmi <shahzodmirzo89@gmail.com>
2025-02-11 15:12:15 +0400

    Add files via upload

--- Snake/src/App.java
+++ Snake/src/App.java
@@ -1,5 +1,11 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Shah!");
+        JFrame frame = new JFrame("Snake duo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*frame.setUndecorated(false);*/
+        frame.setSize(1000, 1000);
+        frame.setVisible(true);
     }
 }
